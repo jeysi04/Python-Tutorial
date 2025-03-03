c0135c1014452c83064c774d82f1cd01e4498e9f
Jaycee Cadag <cadagjaycee37@gmail.com>
2025-03-03 13:13:30 +0800

    Indentationa and Input

---
+++ LessonTwo.py
@@ -0,0 +1,12 @@
+#Indentation in Python
+
+#example in selection
+x = 10
+if x < 20:
+    print(x, 'is less than 20')
+    
+#Without the identation, it would lead to syntax error
+
+#Asking user an input
+name = input("Please enter your name: ")
+print('Your name is', name)
