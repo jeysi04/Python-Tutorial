f0c23f517290baaab3aa9f1eecf656d30c44f4c9
Jaycee Cadag <cadagjaycee37@gmail.com>
2025-03-03 12:54:39 +0800

    Adding files

---
+++ DataType.py
@@ -0,0 +1,25 @@
+#Trying data types
+
+
+#Python has data taypes but unlike C or Java, we don't need to write the data types beside
+# the variable name
+
+#Integer
+x = 10
+print(x)
+
+#Float
+f = 14.24
+print(f)
+
+#Strings
+name1 = 'Elena Aldeguar'
+name2 = "Elene Santos"
+print(name1)
+print(name2)
+
+#characters
+ch = 'A'
+print(ch)
+
+print("Gumana ka naaaaa")
---
+++ LessonOne.py
@@ -0,0 +1,9 @@
+#Practicing the fundamentals in Python
+
+#The function print() can be written with ''/""
+print('Hi')
+print("Hello")
+
+#Assigning variables
+x = 0
+print(x)
